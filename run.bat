76bff72960de9809b43201408860491ebf0c6cd5
David Beckwitt <David.Beckwitt@gmail.com>
2025-06-05 10:01:08 -0500

    Fixed run.bat

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@
 cd /d "%~dp0"
 
 :: 2.  Use the interpreter on PATH
-python -m macro_manager %*
+python -m streamlit run macro_manager/app.py %*
 
 :done
 echo --------------------------------------------------
